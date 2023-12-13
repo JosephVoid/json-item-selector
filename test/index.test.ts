e074b2fc79d135353511dc5bc51d6633ede85e5f
Yoseph <yosephten@gmail.com>
2023-12-13 13:53:46 +0300

    `refactor`: scoped the objects in test functions

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import test_json_2 from "./test-2.json"
 import {JsonItemSelector} from "../src/index"
 
 describe("Access Value", () => {
-    test("Value", () => {
+    test("access_value", () => {
         expect(JsonItemSelector['access_value'](["quiz", "sport", "q1"], test_json_1, 0)?.question)
             .toBe('Which one is correct team name in NBA?');
         expect(JsonItemSelector['access_value'](["quiz", "maths", "q2"], test_json_1, 0)?.answer)
@@ -40,21 +40,26 @@ describe ("Reset & Clearing", () => {
 })
 
 describe("list_options", () => {
-    const JIS_1 = new JsonItemSelector(test_json_1);
-    const JIS_2 = new JsonItemSelector(test_json_2);
-    
     test("list_options length", () => {
+        const JIS_1 = new JsonItemSelector(test_json_1);
+        const JIS_2 = new JsonItemSelector(test_json_2);
+
         expect(JIS_1.list_options().length).toBe(1);
         expect(JIS_2.list_options().length).toBe(13);
     })
 
     test("list_options content", () => {
+        const JIS_1 = new JsonItemSelector(test_json_1);
+        const JIS_2 = new JsonItemSelector(test_json_2);
+
         expect(JIS_1.list_options()[0]).toBe("quiz");
         expect(JIS_2.list_options()[0]).toBe("ADDIS ABABA");
         expect(JIS_2.list_options()[12]).toBe("TIGRAY");
     })
 
     test("list_options array cases", () => {
+        const JIS_2 = new JsonItemSelector(test_json_2);
+
         expect(JIS_2.select_option(JIS_2.list_options()[0])).toBe(true);
         expect(JIS_2.list_options().length).toBe(11);
         expect(JIS_2.list_options()[0]).toBe("ADDIS KETEMA SUB CITY");
@@ -65,6 +70,8 @@ describe("list_options", () => {
     })
 
     test("list_options nested", () => {
+        const JIS_1 = new JsonItemSelector(test_json_1);
+
         expect(JIS_1.select_option(JIS_1.list_options()[0])).toBe(true);
         expect(JIS_1.list_options().length).toBe(2);
         expect(JIS_1.list_options()[0]).toBe("sport");
@@ -77,10 +84,10 @@ describe("list_options", () => {
 })
 
 describe("select_option", () => {
-    const JIS_1 = new JsonItemSelector(test_json_1);
-    const JIS_2 = new JsonItemSelector(test_json_2);
-
     test("select_option selects", () => {
+        const JIS_1 = new JsonItemSelector(test_json_1);
+        const JIS_2 = new JsonItemSelector(test_json_2);
+
         expect(JIS_1.select_option(JIS_1.list_options()[0])).toBe(true);
         expect(JIS_1.list_options().length).toBe(2);
         expect(JIS_1.list_options()[0]).toBe("sport");
