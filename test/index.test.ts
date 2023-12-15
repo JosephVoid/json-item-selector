5eac10a20bb65e8dd1b6559abaf6ad8cc8c1db8d
Yoseph <yosephten@gmail.com>
2023-12-15 23:07:55 +0300

    `test`: object nested type json addition and failure cases added

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,8 @@
 import test_json_1 from "./test-1.json"
 import test_json_2 from "./test-2.json"
+import test_json_3 from "./test-3.json"
+import test_json_4 from "./test-4.json"
+
 import {JsonItemSelector} from "../src/index"
 
 describe("Access Value", () => {
@@ -81,6 +84,34 @@ describe("list_options", () => {
         expect(JIS_1.list_options()[0]).toBe("question");
 
     })
+
+    test("list_option overflow", () => {
+        const JIS_4 = new JsonItemSelector(test_json_4);
+
+        expect(JIS_4.select_option("addis")).toBe(true);
+        expect(JIS_4.select_option("addis")).toBe(true);
+        expect(JIS_4.get_all_selected()).toEqual(["addis", "addis"]);
+        expect(JIS_4.list_options().length).toBe(1);
+        expect(JIS_4.select_option("name")).toBe(true);
+        expect(JIS_4.get_all_selected()).toEqual(["addis", "addis", "name"]);
+        expect(JIS_4.list_options().length).toBe(0);
+
+    })
+
+    test("Alternative JSON", () => {
+        const JIS_3 = new JsonItemSelector(test_json_3);
+        expect(JIS_3.list_options().length).toBe(1);
+        expect(JIS_3.select_option(JIS_3.list_options()[0])).toBe(true);
+        expect(JIS_3.list_options().length).toBe(2);
+        expect(JIS_3.select_option(JIS_3.list_options()[0])).toBe(true);
+        expect(JIS_3.list_options().length).toBe(2);
+        expect(JIS_3.select_option(JIS_3.list_options()[0])).toBe(true);
+        expect(JIS_3.list_options().length).toBe(2);
+        expect(JIS_3.select_option(JIS_3.list_options()[1])).toBe(true);
+        expect(JIS_3.list_options().length).toBe(14);
+        expect(JIS_3.list_options()[8]).toBe("WOREDA 9");
+        expect(JIS_3.select_option(JIS_3.list_options()[8])).toBe(true);
+    })
 })
 
 describe("select_option", () => {
@@ -95,6 +126,33 @@ describe("select_option", () => {
         expect(JIS_2.select_option(JIS_2.list_options()[0])).toBe(true);
         expect(JIS_2.list_options().length).toBe(11);
         expect(JIS_2.list_options()[0]).toBe("ADDIS KETEMA SUB CITY");
+        expect(JIS_2.select_option(JIS_2.list_options()[0])).toBe(true);
+        expect(JIS_2.list_options().length).toBe(14);
+        expect(JIS_2.select_option(JIS_2.list_options()[0])).toBe(true);
+        expect(JIS_2.get_all_selected().length).toBe(3);
+        expect(JIS_2.get_all_selected()).toEqual(["ADDIS ABABA", "ADDIS KETEMA SUB CITY", "WOREDA 1"]);
+    })
+
+    test("select_option Not exisiting cases", () => {
+        const JIS_1 = new JsonItemSelector(test_json_1);
+        expect(JIS_1.select_option("Not existing")).toBe(false);
+        expect(JIS_1.list_options().length).toBe(1);
+        expect(JIS_1.list_options()[0]).toBe("quiz");
+
+    })
+    
+    test("select_option Repeatetion", () => {
+        const JIS_4 = new JsonItemSelector(test_json_4);
+
+        expect(JIS_4.select_option("addis")).toBe(true);
+        expect(JIS_4.select_option("addis")).toBe(true);
+        expect(JIS_4.get_all_selected()).toEqual(["addis", "addis"]);
+        expect(JIS_4.list_options().length).toBe(1);
+
+        expect(JIS_4.clear_all_selected()).toBe(true);
+        expect(JIS_4.select_option("addis", true)).toBe(true);
+        expect(JIS_4.select_option("addis", true)).toBe(false);
+        expect(JIS_4.get_all_selected()).toEqual(["addis"]);
     })
 })
 
